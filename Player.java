1b68def164caa81f8f36cd01385b7ac9642e48d8
Sebastian Iturregui-Shelton <sebastianiturreguishelton@gmail.com>
2016-01-08 12:31:44 -0500

    create two players when numOfPlayers = 2

--- Player.java
+++ Player.java
@@ -1,16 +1,17 @@
 public class player{//creates player object
     int[] hand;
-    int numWheat = 0;
+    int numGrain = 0;
     int numBricks = 0;
     int numWool = 0;
     int numWood = 0;
     int numOre = 0;
-    int[numWheat, numBricks, numWool, numWood, numOre] hand;
+    int[numGrain, numBricks, numWool, numWood, numOre] hand;
     
     public static void main(String[]args){
 	if (numOfPlayers = 2){
-	    player1 = new player[
+	    int[] player1 = new player[];
+	    int[] player2 = new player[];
+	}
     }
-
 }
 
