685bbe57d06d4643b69981a1113e84669e2e43a6
Sebastian Iturregui-Shelton <sebastianiturreguishelton@gmail.com>
2016-01-08 12:27:43 -0500

    created player class and instantiated variables

---
+++ Player.java
@@ -0,0 +1,16 @@
+public class player{//creates player object
+    int[] hand;
+    int numWheat = 0;
+    int numBricks = 0;
+    int numWool = 0;
+    int numWood = 0;
+    int numOre = 0;
+    int[numWheat, numBricks, numWool, numWood, numOre] hand;
+    
+    public static void main(String[]args){
+	if (numOfPlayers = 2){
+	    player1 = new player[
+    }
+
+}
+
