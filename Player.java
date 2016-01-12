7a726d487b97eb9a60a9cf404f7cca4fd4ade887
Sebastian Iturregui-Shelton <sebastianiturreguishelton@gmail.com>
2016-01-12 12:22:03 -0500

    initalized new player arrays with empty hands, of which the indeces are labeled

--- Player.java
+++ Player.java
@@ -1,16 +1,16 @@
 public class player{//creates player object
     int[] hand;
-    int numGrain = 0;
-    int numBricks = 0;
-    int numWool = 0;
-    int numWood = 0;
-    int numOre = 0;
-    int[numGrain, numBricks, numWool, numWood, numOre] hand;
+    //hand[0] is Brick
+    //hand[1] is Wool
+    //hand[2] is Ore
+    //hand[3] is Grain
+    //hand[4] is Lumber
     
     public static void main(String[]args){
-	if(numOfPlayers = 2){
-	    int[] player1 = new player[];
-	    int[] player2 = new player[];
+	int numOfPlayers = 2;//will be set to a variable coming from the button
+	if(numOfPlayers = 2){//requires button "number of players(2)"  to set numOfPlayers to 2
+	    int[] player1 = new player[4];
+	    int[] player2 = new player[4];
 	}
     }
 }
