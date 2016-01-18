74afd71ae6e72cb220722aeb8a5417bb859dfe4d
Chang Ju Kim <kimchangju@yahoo.com>
2016-01-18 17:54:50 -0500

    Create buttons, create map on setup. Make it easy to add features when buttons are pressed. Found a bug with buttons when pressed. Coded a fourth of mouseclick, (for when settlements cities and roads are built) but still have a lot of work left.

--- CreateMap/initWorld.java
+++ CreateMap/initWorld.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 
 import javax.swing.*;
 
-public class initWorld extends JPanel implements ActionListener{
+public class initWorld extends JPanel implements ActionListener, MouseListener{
+    //removes serial warning
+    private static final long serialVersionUID = 123;
+
     int[][] gridPoint = MakeTiles.pointGrid(51);
-    Coordinate[] points = MakeTiles.points(gridPoint, 450, 75);
+    Coordinate[] points = MakeTiles.points(gridPoint, 500, 75);
     Hexagon[] hexes = MakeTiles.makeHexagon(points);
 
-    /*
-      only calls paint once... thus cannot update for new settlements/cities
-      may want to call repaint in paint instead. Might make game a bit slow, but can now be updated.
-    */
-
+    
     public void paintComponent(Graphics g) {
+	System.out.println("painted!");
 	g.setColor(Color.BLACK);
 	for (int h = 0; h < hexes.length; h++) {
 	    for (int l = 0; l < 6; l++) {
@@ -27,9 +27,11 @@ public class initWorld extends JPanel implements ActionListener{
 	    //the color scheme needs to be fixed (players need to know whose settlement is whose
 	    //may instead use filled rectangles in filled rectangles to indicate cities/settlements
 
-	    //tests
-	    //points[i].setSettlement(true);
-	    //points[i].setCity(true);
+	    /*
+	    tests
+	    points[i].setSettlement(true);
+	    points[i].setCity(true);
+	    */
 	    
 	    //settlement = red rectangle
 	    if (points[i].hasSettlement()) {
@@ -43,46 +45,66 @@ public class initWorld extends JPanel implements ActionListener{
 	    }
 	}
     }
+
+    //mouse actions
+    public void mouseEntered(MouseEvent e) { }
+    public void mouseExited(MouseEvent e) { }
+    public void mousePressed(MouseEvent e) { }
+    public void mouseReleased(MouseEvent e) { }
+    public void mouseClicked(MouseEvent e) {
+	
+    }
+
     
     //Creates buttons, world, etc.
 
     public void actionPerformed(ActionEvent e) {
-	if (e.getSource() == buyBox) {
-	    String thingToBuy = (String)((JComboBox)e.getSource()).getSelectedItem();
+	if (e.getSource() == buy) {
+	    System.out.println("pressed buy!");
+	    String thingToBuy = (String)buyBox.getSelectedItem();
 	    if (thingToBuy.equals("Road")) {
-		//add code here
-		int x = 5;
+	        buyMessage.setText("You bought a road!");
 	    } else if (thingToBuy.equals("Settlement")) {
-		//add code here
+		buyMessage.setText("You bought a settlement!");
 	    } else if (thingToBuy.equals("City")) {
-		//add code here
+		buyMessage.setText("You bought a city!");
 	    } else {
-		//add code here
+		buyMessage.setText("");
 	    }
+	}
+	if (e.getSource() == endTurn) {
+	    //end the player's turn
 	}
     }
     
     JButton endTurn = new JButton("End turn?");
     
     String[] choices = {"Road", "Settlement", "City"};
-    JComboBox buyBox = new JComboBox(choices);
+    JComboBox<String> buyBox = new JComboBox<String>(choices);
     JButton buy = new JButton("Buy!");
-    
+    JLabel buyMessage = new JLabel();
 
     public initWorld() {
 	buyBox.addActionListener(this);
-	add(endTurn);
+	buy.addActionListener(this);
 
-	this.add(buyBox);
-	this.add(buy);
-	this.add(endTurn);
-	this.setComponentZOrder(buyBox, 0);
-	this.setComponentZOrder(buy, 0);
-	this.setComponentZOrder(endTurn, 0);
+	endTurn.addActionListener(this);
+
+	this.add(buyBox, BorderLayout.LINE_START);
+
+	this.add(buy, BorderLayout.CENTER);
+
+	this.add(buyMessage, BorderLayout.LINE_START);
+
+	this.add(endTurn, BorderLayout.LINE_START);
+	System.out.println("added buttons!");
+
+	addMouseListener(this);
     }
+
     
     public static void main(String[]args) {
-	initWorld p = new initWorld();
+      	initWorld p = new initWorld();
 
 	JFrame f = new JFrame();
 	f.setVisible(true);
@@ -90,7 +112,5 @@ public class initWorld extends JPanel implements ActionListener{
 	f.add(p);
 	f.setSize(1000, 600);
 	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	p.repaint(450, 75, 600, 600);
-
     }
 }
